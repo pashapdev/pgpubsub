7a503fc4b5a668b30ebd9dd0dd0707c79806600d
Pavel Popov <pavel.popov@aviasales.ru>
2021-05-02 23:47:34 +0300

    add notify

---
+++ pkg/queue/notify.go
@@ -0,0 +1,18 @@
+package queue
+
+type Notify struct {
+	Messages chan string
+	Err      chan error
+}
+
+func NewNotify() *Notify {
+	return &Notify{
+		Messages: make(chan string),
+		Err:      make(chan error),
+	}
+}
+
+func (n *Notify) Close() {
+	close(n.Messages)
+	close(n.Err)
+}
